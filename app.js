734db2adab5e974d15a4135d2eec23533bf53dfb
Atul Raj <raazatul7@gmail.com>
2020-07-08 13:07:08 +0530

    'initial'

---
+++ app.js
@@ -0,0 +1,60 @@
+const express = require("express");
+const app = express();
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+const productRoutes = require("./api/routes/products");
+const orderRoutes = require("./api/routes/orders");
+
+mongoose.connect(
+  "mongodb+srv://anjali:" +
+    process.env.MONGO_ATLAS_PW +
+    "@node-rest-shop.wf7fj.mongodb.net/anjali?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
+// .then(() => console.log("MongoDB connected..."))
+// .catch((err) => console.log(err));
+
+mongoose.Promise = global.Promise;
+
+app.use(morgan("dev"));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  req.header("Access-Control-Allow-Origin", "*");
+  req.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  if (req.method === "OPTION") {
+    req.header("Acess-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+    return res.json(200).json({});
+  }
+  next();
+});
+
+// Routes which should handle request
+app.use("/products", productRoutes);
+app.use("/orders", orderRoutes);
+
+app.use((req, res, next) => {
+  const error = new Error("not found");
+  error.status = 404;
+  next(error);
+});
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message,
+    },
+  });
+});
+
+module.exports = app;
